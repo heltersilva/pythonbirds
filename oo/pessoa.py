9a94ad4985c2a98fc58083d2658fef7e6a6b4f62
helter <helter.silva13@gmail.com>
2020-03-24 15:43:47 -0300

    Atualização na classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,22 @@
 class   Pessoa:
+    def __init__(self, *filhos, nome=None, idade=25):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+    luciano.sobrenome = 'Ramalho'
+    del luciano.filhos
+    print(renzo.__dict__)
+    print(luciano.__dict__)
